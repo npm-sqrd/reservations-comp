13883215f9382dfedd75746e96e149a7ff9b4cb0
Mat Bagnall <matbagnall@gmail.com>
2018-03-23 16:50:11 -0700

    slight change to artillery process

--- sdc_files/artillery/pgArtilleryRun.js
+++ sdc_files/artillery/pgArtilleryRun.js
@@ -1,15 +1,18 @@
-const gen = require('../postgres/pgDataGen');
-const { exec } = require('child_process');
+// const gen = require('../postgres/pgDataGen');
+// const { exec } = require('child_process');
 
-// generating 1 million records for artillery csv file
-gen.reservationsList(7e6, 'pg', (data) => {
-  if (data) {
-    exec('artillery run ./sdc_files/artillery/pgConfig.yml', (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(result);
-      }
-    });
-  }
-});
+// // generating 1 million records for artillery csv file
+// gen.reservationsList(7e6, 'pg', (data) => {
+//   if (data) {
+//     exec('artillery run ./sdc_files/artillery/pgConfig.yml', (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log(result);
+//       }
+//     });
+//   }
+// });
+
+
+// *********** This file is in case I decide to try and use payloads for testing ***** //
