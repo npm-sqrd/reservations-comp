3776161010bf581ec348ec84347186e9099d579b
Mat Bagnall <matbagnall@gmail.com>
2018-03-24 16:33:56 -0700

    update index.html to render bundle once received from client

--- sdc_files/nodeServer.js
+++ sdc_files/nodeServer.js
@@ -10,19 +10,18 @@ http.createServer((req, res) => {
   const { method, url } = req;
   const id = url.split('/')[2];
   const date = url.split('/')[4];
-  console.log(url);
   if (method === 'GET' && url === '/') {
     const staticStream = fs.createReadStream(path.join(__dirname, '../client/dist/index.html'), 'utf8');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     staticStream.pipe(res);
-  } else if (method === 'GET' && /*url === '/bundle.js'*/ url.match('.js')) {
+  } else if (method === 'GET' && url === '/bundle.js') {
     const stream = fs.createReadStream(path.join(__dirname, '../client/dist/bundle.js'), 'utf8');
     res.writeHead(200, { 'Content-Type': 'text/javascript' });
     stream.pipe(res);
-  // } else if (method === 'GET' && url === '/bundle-server.js') {
-  //   const stream = fs.createReadStream(path.join(__dirname, '../client/dist/bundle-server.js'), 'utf8');
-  //   res.writeHead(200, { 'Content-Type': 'text/javascript' });
-  //   stream.pipe(res);
+  } else if (method === 'GET' && url === '/bundle-server.js') {
+    const stream = fs.createReadStream(path.join(__dirname, '../client/dist/bundle-server.js'), 'utf8');
+    res.writeHead(200, { 'Content-Type': 'text/javascript' });
+    stream.pipe(res);
   } else if (method === 'GET' && url === `/restaurants/${id}/reservations/${date}`) {
     db.genReservationSlots(id, date, (err, data) => {
       if (err) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
 
+const common = {
+  context: path.join(__dirname, '/client/src/components'),
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
 const client = {
-  entry: './client/src/clientView.js',
+  entry: path.join(__dirname, '/client/src/serverView.js'),
   module: {
     rules: [
       {
@@ -26,23 +33,17 @@ const client = {
     ]
   },
 
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'client/dist'),
-    publicPath: '/'
+    path: path.join(__dirname, '/client/dist')
   }
 };
 
 const server = {
-  context: path.resolve(__dirname, 'client'),
-  entry: path.resolve(__dirname, 'client/src/serverView.js'),
+  entry: path.join(__dirname, '/client/src/clientView.js'),
   target: 'node',
   output: {
-    path: path.resolve(__dirname, 'client/dist'),
+    path: path.join(__dirname, '/client/dist'),
     filename: 'bundle-server.js',
     libraryTarget: 'commonjs-module',
   },
@@ -68,13 +69,51 @@ const server = {
         loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
       },
     ]
-  },
-
-  resolve: {
-    extensions: ['.js', '.jsx']
   }
 };
 
 module.exports = [
-  client, server,
+  Object.assign({}, common, client),
+  Object.assign({}, common, server),
 ];
+
+// const path = require('path');
+
+// module.exports = {
+//   entry: './client/src/index.jsx',
+
+
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /(node_modules|bower_components)/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['env', 'react']
+//           }
+//         }
+//       },
+//       {
+//         test: /dayPicker\.css$/,
+//         use: [ 'style-loader', 'css-loader' ],
+//       },
+//       {
+//         test: /\.css$/,
+//         exclude: /dayPicker\.css$/,
+//         loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+//       },
+//     ]
+//   },
+
+//   resolve: {
+//     extensions: ['.js', '.jsx']
+//   },
+
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'client/dist'),
+//     publicPath: '/'
+//   }
+// }
